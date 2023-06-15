7ad2b81c18ba993376a92a1deaa9723a84bca629
lena <107754913+leonorlyn@users.noreply.github.com>
2023-06-15 13:41:34 -0700

    Author Route

--- routes/authors.js
+++ routes/authors.js
@@ -1,48 +1,105 @@
-const {formatMuiErrorMessage } = require('@mui/utils')
 const express = require('express')
 const router = express.Router()
 const Author = require('../models/author')
+const Book = require('../models/book')
 
+// All Authors Route
+router.get('/', async (req, res) => {
+  let searchOptions = {}
+  if (req.query.name != null && req.query.name !== '') {
+    searchOptions.name = new RegExp(req.query.name, 'i')
+  }
+  try {
+    const authors = await Author.find(searchOptions)
+    res.render('authors/index', {
+      authors: authors,
+      searchOptions: req.query
+    })
+  } catch {
+    res.redirect('/')
+  }
+})
 
 // New Author Route
 router.get('/new', (req, res) => {
-    res.render('authors/new', { author: new Author() })
-    // 如果网页到了authors/new page（也就是user发出get的这个req），那么网页将render（present）authors/new.ejs
+  res.render('authors/new', { author: new Author() })
 })
 
-
-//All Author Route
-router.get('/', async (req, res) => {
-    let searchOptions = {}
-    if(req.query.name != null && req.query.name != ''){
-        searchOptions.name = new RegExp(req.query.name, 'i')
-    }
-    try{
-        const authors = await Author.find(searchOptions)
-        res.render('authors/index', {
-            authors : authors, 
-            searchOptions: req.query
-        })
-    }catch{
-        res.redirect('/')
-    }
-})
-
-// create Author Route
+// Create Author Route
 router.post('/', async (req, res) => {
-    const author = new Author({
-        name: req.body.name
+  const author = new Author({
+    name: req.body.name
+  })
+  try {
+    const newAuthor = await author.save()
+    res.redirect(`authors/${newAuthor.id}`)
+  } catch {
+    res.render('authors/new', {
+      author: author,
+      errorMessage: 'Error creating Author'
     })
-    try{
-        const newAuthor = await author.save()
-        res.redirect('authors')
-    }catch{
-        res.render('authors/new', {
-        author: author,
-        errorMessage : `Error Creating Author...` 
-        })
-    }
+  }
 })
 
+// Show the books for selected author
+router.get('/:id', async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id)
+    const books = await Book.find({ author: author.id }).limit(6).exec()
+    res.render('authors/show', {
+      author: author,
+      booksByAuthor: books
+    })
+  } catch {
+    res.redirect('/')
+  }
+})
+
+// Edit author
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id)
+    res.render('authors/edit', { author: author })
+  } catch {
+    res.redirect('/authors')
+  }
+})
+
+
+// Show one author page
+router.put('/:id', async (req, res) => {
+  let author
+  try {
+    author = await Author.findById(req.params.id)
+    author.name = req.body.name
+    await author.save()
+    res.redirect(`/authors/${author.id}`)
+  } catch {
+    if (author == null) {
+      res.redirect('/')
+    } else {
+      res.render('authors/edit', {
+        author: author,
+        errorMessage: 'Error updating Author'
+      })
+    }
+  }
+})
+
+// Delete the author
+router.delete('/:id', async (req, res) => {
+  let author
+  try {
+    author = await Author.findById(req.params.id)
+    await author.remove()
+    res.redirect('/authors')
+  } catch {
+    if (author == null) {
+      res.redirect('/')
+    } else {
+      res.redirect(`/authors/${author.id}`)
+    }
+  }
+})
 
 module.exports = router
